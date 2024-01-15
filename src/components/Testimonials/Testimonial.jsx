ec0615a33defc066e04d55249dbba469d8588d3d
subashree3456 <107339951+subashree3456@users.noreply.github.com>
2024-01-15 19:59:51 +0530

    Add files via upload

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jpg";
+import profilePic2 from "../../img/profile2.jpg";
+import profilePic3 from "../../img/profile3.jpg";
+import profilePic4 from "../../img/profile4.jpg";
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      review:
+        " Received GEM awards for two times for the team best performer and also got appreciation for my work",
+    },
+    {
+      img: profilePic2,
+      review:
+        "Rewarded by the clients and Director with the appreciation mail for the work excellence towards successful delivery of the projects within scheduled time",
+    },
+    {
+      img: profilePic3,
+      review:
+        "Received Star of the Month Award for 3 times and On the Spot Award for 2 times for the best performer",
+    },
+    {
+      img: profilePic4,
+      review:
+        "Received Service & Commitment Award for implemented new procedures on the Project ",
+    },
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+      <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
+      <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
